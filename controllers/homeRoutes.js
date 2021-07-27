bcf328c4fb042b16bde62ff3701ae55d7e4d4b2f
Emma Mattner <emattner1997@gmail.com>
2021-07-27 16:52:58 +0930

    renders posts to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,33 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
+
+router.get("/", async (req, res) => {
+    console.log(req.session);
+
+    const post = await Post.findAll({
+        attributes: ["id", "post_text", "title", "created_at"],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"],
+                },
+            },
+            {
+                model: User,
+                attributes: ["username"],
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+            // Pass serialized data and session flag into template
+            res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
