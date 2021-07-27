c9e1a4429bf2c21765c165529ede58c0389007ef
Emma Mattner <emattner1997@gmail.com>
2021-07-27 16:53:47 +0930

    render login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -31,3 +31,11 @@ router.get("/", async (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+    res.render("login");
+});
