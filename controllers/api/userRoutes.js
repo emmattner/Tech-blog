04e471c473527a4843e89ec8ea04e07a1f9cc0ca
Emma Mattner <emattner1997@gmail.com>
2021-07-27 17:07:39 +0930

    gets access to users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
-const withAuth = require("../../utils/auth");
+const withAuth = require("../../utils/withAuth");
+
+router.get("/", async (req, res) => {
+    const user = await User.findAll({
+        attributes: { exclude: ["[password"] },
+    })
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});ß
