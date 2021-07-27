8c390c0259ab8b109eedfdc3d5f91a2bb694ec09
Emma Mattner <emattner1997@gmail.com>
2021-07-27 21:29:45 +0930

    has many comment

--- models/index.js
+++ models/index.js
@@ -11,3 +11,25 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
     foreignKey: "user_id",
 });
+
+// a comment can only belong to one user
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+// a comment can only belong to one user
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+});
+
+// users can make many comments
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+});
+
+// users can make many posts
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
