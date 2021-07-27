b19f3d9e2e59d44f30b479f1047b44b57a950884
Emma Mattner <emattner1997@gmail.com>
2021-07-27 21:30:51 +0930

    added comment

--- models/index.js
+++ models/index.js
@@ -17,12 +17,12 @@ Comment.belongsTo(User, {
     foreignKey: "user_id",
 });
 
-// a comment can only belong to one user
+// a post can only belong to one user
 Comment.belongsTo(Post, {
     foreignKey: "post_id",
 });
 
-// users can make many comments
+// allows users to make many comments
 User.hasMany(Comment, {
     foreignKey: "user_id",
 });
