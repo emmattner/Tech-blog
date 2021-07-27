e2c6e87f5509ce3a15697b7f1ed0e8b183825414
Emma Mattner <emattner1997@gmail.com>
2021-07-27 16:59:19 +0930

    delete comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -45,3 +45,17 @@ router.put("/", withAuth, async (req, res) => {
         });
 });
 
+router.delete("/:id", withAuth, async (req, res) => {
+    const comment = await Comment.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
