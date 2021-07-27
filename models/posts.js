ee18a97a4ea7e9bd1bda3ae20030dcd69cba25de
Emma Mattner <emattner1997@gmail.com>
2021-07-27 21:32:49 +0930

    creating table for posts

---
+++ models/posts.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model { }
+// Defines table fields
+Post.init(
+    {
+        // Creates ID Column (Primary Key)
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+    },
+
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post",
+    }
+);
+
+module.exports = Post;
