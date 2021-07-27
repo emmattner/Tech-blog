5e5531f73e723c318fa49152589059fa2e835c6f
Emma Mattner <emattner1997@gmail.com>
2021-07-27 16:17:49 +0930

    get route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+router.get("/", async (req, res) => {
+    const comment = await Comment.findAll({})
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
