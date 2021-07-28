a7b2d7dd396c0faa5bc7843eed40628b94921de4
Emma Mattner <emattner1997@gmail.com>
2021-07-28 21:15:06 +0930

    delete post

---
+++ public/js/deletePost.js
@@ -0,0 +1,24 @@
+
+const delButtonHandler = async (event) => {
+    event.preventDefault();
+
+    if (event.target.hasAttribute("data-id")) {
+        const id = event.target.getAttribute("data-id");
+
+        const response = await fetch(`/api/posts/${id}`, {
+            method: "DELETE",
+            body: JSON.stringify({
+                post_id: id,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace("/dashboard");
+        } else {
+            alert("Failed to delete post");
+        }
+    }
+};
