f41b7d5eb54bd6e28255b70ff8d44871ca9fae2b
Emma Mattner <emattner1997@gmail.com>
2021-07-28 21:25:03 +0930

    post request

---
+++ public/js/signUp.js
@@ -0,0 +1,17 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect values from the login form
+    const username = document.querySelector("#username-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+
+    if (username && email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch("/api/users", {
+            method: "POST",
+            body: JSON.stringify({ username, email, password }),
+            headers: { "Content-Type": "application/json" },
+        });
+    }
+};
