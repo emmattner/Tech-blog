8bba8bb0a586ebfb0ae9786ac5ca1e94e1d01e26
Emma Mattner <emattner1997@gmail.com>
2021-07-28 21:18:18 +0930

    put request

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -8,4 +8,25 @@ const editFormHandler = async (event) => {
     const id = window.location.toString().split("/")[
         window.location.toString().split("/").length - 1
     ];
+
+    if (title && post_text && id) {
+        // send a PUT request to the API endpoint
+        const response = await fetch(`/api/posts/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({
+                title,
+                post_text,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            // If successful, redirect the browser to the dashboard
+            document.location.replace("/dashboard/");
+        } else {
+            alert(response.statusText);
+        }
+    }
 };    
