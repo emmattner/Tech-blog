c37f0207ae761744b45bd35c959c84c897c13b18
Emma Mattner <emattner1997@gmail.com>
2021-07-28 21:09:55 +0930

    post request

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -4,3 +4,24 @@ const newFormHandler = async (event) => {
     // Collect values from the form
     const title = document.querySelector('[name="post-title"]').value;
     const post_text = document.querySelector('[name="post-text"]').value;
+
+    if (title && post_text) {
+        const response = await fetch(`/api/posts`, {
+            method: "POST",
+            body: JSON.stringify({
+                title,
+                post_text,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            // If successful, redirects browser to dashboard page
+            document.location.replace("/dashboard/");
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
